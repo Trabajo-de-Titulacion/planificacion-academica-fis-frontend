4d7dc2490c753fed96e875bc91eb093d10d85883
Mahatma Quijano <mahatma.quijano@epn.edu.ec>
2022-08-16 17:41:04 -0500

    feat: Mejora en la guarda de login

--- SPA-Frontend/src/app/servicios/auth/guards/logged.guard.ts
+++ SPA-Frontend/src/app/servicios/auth/guards/logged.guard.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
+import { catchError, map, Observable, of } from "rxjs";
 import Swal from "sweetalert2";
+import { AuthService } from "../auth.service";
 import { TokenStorageService } from "../token-storage.service";
 
 @Injectable()
@@ -10,28 +11,49 @@ export class LoggedGuard implements CanActivate{
     constructor(
         private readonly tokenService: TokenStorageService,
         private readonly router: Router,
+        private readonly authService: AuthService,
     ) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        const sesionIniciada = !!this.tokenService.obtenerToken();
+        // Se obtiene el token del storage
+        const token = this.tokenService.obtenerToken();
 
-        if (!sesionIniciada) {
-            const toast = Swal.mixin({
-                toast: true,
-                position: 'top-end',
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: false,
-            })
-            
-            this.router.navigate(['/login']);
-        
-            toast.fire({
-                icon: 'error',
-                title: '¡Acceso denegado!'
-            });
+        // Si no hay token almacenado
+        if (!token) {
+            return false;
         }
-        return sesionIniciada;
+
+        // Se verifica la validez del token
+        return this.authService.verificarToken(token).pipe(
+            // Si es válido
+            map( () => {
+                return true;
+            }),
+            // Si no es válido
+            catchError( error => {
+                // Vaciar sesión
+                this.tokenService.cerrarSesion();
+
+                // Redirigir al login
+                this.router.navigate(['/login']);
+
+                // Mensaje de error
+                const toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: false,
+                });
+            
+                toast.fire({
+                    icon: 'error',
+                    title: '¡Acceso denegado!'
+                });
+
+                return of(false);
+            })
+        );
     }
 
 }
