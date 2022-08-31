1f4df16e3e24645758334c681e2482e202d52602
Mahatma Quijano <mahatma.quijano@epn.edu.ec>
2022-08-31 17:37:19 -0500

    feat(Horas no disponibles): Se crea el rol jefe de departamento

--- SPA-Frontend/src/app/servicios/auth/guards/es-docente.guard.ts
+++ SPA-Frontend/src/app/servicios/auth/guards/es-docente.guard.ts
@@ -1,18 +1,26 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { RolesEnum } from "../enum/roles.enum";
 import { UsuarioStorageService } from "../usuario-storage.service";
 
 @Injectable()
-export class EsDocenteGuard implements CanActivateChild{
+export class EsDocenteGuard implements CanActivate, CanActivateChild{
 
     constructor(
         private readonly userService: UsuarioStorageService,
         private readonly router: Router,
     ) {}
+    
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        return this.tieneRolDocente();
+    }
 
     canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        return this.tieneRolDocente();
+    }
+
+    tieneRolDocente() {
         const roles = this.userService.obtenerRoles();
         if (!roles) {
             return false;
